5a455cffd5baa3ebdf3cbb058fb1e3cc92ba565b
Pralkopodobny <43888534+Pralkopodobny@users.noreply.github.com>
2026-04-09 00:12:26 +0200

    Fix calendar day alignment

--- services/frontend/demdemfront/src/app/components/happiness-calendar-month/happiness-calendar-month.ts
+++ services/frontend/demdemfront/src/app/components/happiness-calendar-month/happiness-calendar-month.ts
@@ -2,6 +2,7 @@ import {Component, computed, input} from '@angular/core';
 import {HappinessCalendarCell} from '../happiness-calendar-cell/happiness-calendar-cell';
 import {Day} from '../../model/Day';
 import {Happiness} from '../../model/Happiness';
+import dayjs from 'dayjs';
 
 @Component({
   selector: 'app-happiness-calendar-month',
@@ -18,17 +19,8 @@ export class HappinessCalendarMonth {
   monthData = computed(() => randomMonth(this.month(), this.year()))
 }
 
-function firstElem(firstWeekdayOfMonth: number): number {
-  switch (firstWeekdayOfMonth) {
-    case 1: return 1 // Monday
-    case 2: return 0 // Tuesday
-    case 3: return -1 // Wednesday
-    case 4: return -2 // Thursday
-    case 5: return -3 // Friday
-    case 6: return -4 // Saturday
-    case 0: return -5 // Sunday
-    default: throw 'Invalid day'
-  }
+function offset(firstWeekdayOfMonth: number): number {
+  return (firstWeekdayOfMonth + 6) % 7
 }
 
 function monthToMonthIndex(month: number): number {
@@ -38,13 +30,11 @@ function monthToMonthIndex(month: number): number {
 
 function randomMonth(month: number, year: number): Day[]  {
   const days = [];
-  const lastDayOfMonth = new Date(year, monthToMonthIndex(month), 0)
-  const firstDayOfMonth = new Date(year, monthToMonthIndex(month), 1)
-  const numberOfDays = lastDayOfMonth.getDate()
-  const first = firstElem(firstDayOfMonth.getDay())
-  console.log(`${month}.${year} -- ${lastDayOfMonth}`)
-  console.log(`${month}.${year} -- ${firstDayOfMonth}`)
-  console.log(first)
+
+  const monthJs = dayjs().month(month - 1).year(year).date(1)
+  const numberOfDays = monthJs.daysInMonth()
+  const firstDay = monthJs.day();
+  const first = 1 - offset(firstDay)
 
   for (let day = first; day <= numberOfDays; day++) {
     days.push({
