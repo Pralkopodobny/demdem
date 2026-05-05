8bfefb457fb963c99a43e865868780963334c4ae
michalliss <michal.j.liss@gmail.com>
2026-05-05 21:22:58 +0200

    rust 2

--- services/backend/mood-rs/src/main.rs
+++ services/backend/mood-rs/src/main.rs
@@ -5,17 +5,20 @@ use crate::models::Mood;
 use axum::extract::State;
 use axum::http::StatusCode;
 use axum::response::IntoResponse;
+use axum::routing::post;
 use axum::{Json, Router, routing::get};
+use chrono::Utc;
 use deadpool::managed;
-use deadpool::managed::Object;
-use diesel::{Connection, PgConnection};
+use diesel::{Connection, PgConnection, insert_into};
 use diesel_async::pooled_connection::AsyncDieselConnectionManager;
 use diesel_async::pooled_connection::deadpool::Pool;
 use diesel_async::{AsyncPgConnection, RunQueryDsl};
 use diesel_migrations::{EmbeddedMigrations, MigrationHarness, embed_migrations};
 use dotenvy::dotenv;
+use serde::{Deserialize, Serialize};
 use std::env;
 use std::sync::Arc;
+use uuid::Uuid;
 
 pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!("migrations");
 
@@ -40,8 +43,14 @@ pub fn establish_single_connection() -> PgConnection {
 
 use diesel::prelude::*;
 
+#[derive(Serialize, Deserialize)]
+struct CreateMood {
+    moodlevel: String,
+}
+
 #[tokio::main]
 async fn main() {
+    println!("Siemano");
     use self::schema::moods::dsl::*;
 
     let c = &mut establish_single_connection();
@@ -76,16 +85,23 @@ async fn main() {
 
     async fn post_moods_handler(
         State(state): State<Arc<AppState>>,
+        Json(payload): Json<CreateMood>,
     ) -> Result<impl IntoResponse, StatusCode> {
+        use schema::moods::dsl::*;
+
         let mut connection = state
             .pool
             .get()
             .await
             .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
 
-        let result = moods
-            .select(Mood::as_select())
-            .load(&mut connection)
+        let result = insert_into(moods)
+            .values((
+                moodlevel.eq(payload.moodlevel),
+                timestamp.eq(Utc::now()),
+                id.eq(Uuid::new_v4()),
+            ))
+            .execute(&mut connection)
             .await
             .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
 
@@ -96,7 +112,7 @@ async fn main() {
     let app = Router::new()
         .route("/", get(|| async { "Hello, World!" }))
         .route("/moods", get(get_moods_handler))
-        .route("/moods/:status", get(post_moods_handler))
+        .route("/moods", post(post_moods_handler))
         .with_state(state);
 
     // run our app with hyper, listening globally on port 3000
