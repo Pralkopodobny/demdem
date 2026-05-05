7edd23ed7df1a4fa6d847d4baece7ee58a5aca11
Pralkopodobny <43888534+Pralkopodobny@users.noreply.github.com>
2026-05-05 22:09:15 +0200

    Use UTC timezone

--- services/frontend/demdemfront/src/main.ts
+++ services/frontend/demdemfront/src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+
+dayjs.extend(utc);
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
